b054121ea6189e08685246d26c0e3123f0b53dc5
Yves <yek@kunzy.net>
2022-11-08 23:02:46 +0100

    moved static variables to implementation

--- nlog.h
+++ nlog.h
@@ -5,7 +5,9 @@
  * this is a header only library. to use it you need
  * to define NLOG_IMPLEMENTATION in exactly one
  * compilation unit before including this file.
+ *
  * */
+
 #pragma once
 
 #define LOG_ERROR 0
@@ -13,14 +15,15 @@
 #define LOG_INFO 2
 #define LOG_DEBUG 3
 
-static unsigned int nlog_level = 0;
-static void (*nlog_printstr)(const char * str) = NULL;
-
 void nlog_init (void (*printstr)(const char*));
 void nlog_set_level (unsigned int level);
 void nlog(unsigned int level, const char* str);
 
+
 #ifdef NLOG_IMPLEMENTATION
+
+static unsigned int nlog_level = 0;
+static void (*nlog_printstr)(const char * str) = NULL;
 
 void nlog_init (void (*printstr)(const char*)) {
     if (printstr != NULL) {
@@ -39,3 +42,4 @@ void nlog(unsigned int level, const char* str) {
 }
 
 #endif
+
