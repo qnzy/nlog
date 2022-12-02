21e47335ec4539059fa5f480f5be8ddb288b3802
Yves <yek@kunzy.net>
2022-12-02 11:15:28 +0100

    added LOG_ALWAYS level, reformatted defines

--- nlog.h
+++ nlog.h
@@ -9,10 +9,11 @@
 
 #pragma once
 
-#define LOG_ERROR 0
-#define LOG_WARNING 1
-#define LOG_INFO 2
-#define LOG_DEBUG 3
+#define LOG_ALWAYS      0
+#define LOG_ERROR       0
+#define LOG_WARNING     1
+#define LOG_INFO        2
+#define LOG_DEBUG       3
 
 void nlog_init (void (*printstr)(const char*));
 void nlog_set_level (unsigned int level);
